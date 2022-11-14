40897add01e7185b51adf0d30546fd51730a97ce
nacho5036 <ssalazarm2000@alumno.ipn.mx>
2022-11-13 20:22:47 -0600

    new

--- app.js
+++ app.js
@@ -3,21 +3,36 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
+app.set('port',process)
+
 app.use(express.static('public'));
-server.listen(8080, () => console.log('Servidor iniciado en 8080'));
+server.listen(3000, () => console.log('Servidor iniciado en 3000'));
 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/public/index.html');
+  res.sendFile(__dirname + 'public');
 });
 
 io.on('connection', function (socket) {
   console.log('socket conectado',socket.id);
-  io.emit('conectado', {texto: 'Nuevo socket conectado: ' + socket.id +`<br>`} );
+  io.emit('conectado', {
+    texto: 'Nuevo socket conectado: ',
+    id: socket.id,
+  });
 
   socket.on('disconnect', () => {
   	console.log('socket desconectado',socket.id);
-    io.emit('desconectado', {texto: 'Socket desconectado.'+ socket.id +`<br>`});
-  
+    io.emit('desconectado', {
+      texto: 'Socket desconectado.', 
+      id: socket.id,
+    });
   });
 
+socket.on('chat:mensaje', (data)=>{
+  io.emit('chat:mensaje', data); 
 });
+
+socket.on('chat:escribiendo', (usuario) =>{
+  socket.broadcast.emit('chat:escribiendo', usuario);
+});
+
+}); 
--- public/js/logica.js
+++ public/js/logica.js
@@ -1,22 +1,52 @@
 //Crear una instancia de SocketIO, recibe como parámetro el url del servidor al que se conectará
-var socket = io.connect('http://localhost:8080');
+var socket = io(); 
+var list = document.querySelector('#not');
 
-var notificaciones = [];
+let mensaje = document.getElementById('mensaje');
+let usuario = document.getElementById('usuario'); 
+let salida = document.getElementById('salida');
+let notificaciones = document.getElementById('notificaciones');
+let boton = document.getElementById('enviar');
 
-//Escuchar al evento 'hola' y realizar cierta accion, recibe como parámetro el id del evento y un callback con la información recibida
-socket.on('conectado', function (data) {
-  //Notificar al usuario el evento hola
-  console.log(data);
-  
-  notificaciones.push(data);
-  document.querySelector('#notificaciones').innerHTML = JSON.stringify(notificaciones);
+var clientes = [];
+
+boton.addEventListener('click', function(){
+  var data = {
+    mensaje: mensaje.value,
+    usuario: usuario.value,
+  };
+
+  if (mensaje.value === '' || usuario.value === ''){
+    alert('Se requiere un mensaje y un usuario para poder igresar al chat');
+  } else {
+    mensaje.value = ''
+    socket.emit('chat:mensaje', data);
+  }
+}); 
+
+mensaje.addEventListener('keydown', function () {
+  socket.emit('chat:escribiendo', usuario.value)
+}); 
+
+
+socket.on('chat:mensaje', function (data) {
+  salida.innerHTML += 
+  '<p><strong>'+ data.usuario + '</strong>:'+ data.mensaje + '</p>'
+  notificaciones.innerHTML = '';
+}); 
+
+socket.on('chat:escribiendo', function (data){
+  notificaciones.innerHTML = '<p><em>' + data + '</em>está escribiendo...</p>';
 });
 
-socket.on('desconectado', function (data) {
-  //Notificar al usuario el evento hola
+socket.on('socket_desconectado', function (data) {
   console.log(data);
-  
-  notificaciones.push(data);
-  document.querySelector('#notificaciones').innerHTML = JSON.stringify(notificaciones);
-});
+  clientes = clientes.filter(function (cliente){
+    return cliente.id != data.id;
+  });
+}); 
 
+socket.on('socket_conectado', function (data){
+  console.log("socket conectado")
+  console.log(data); 
+}); 
